bee0d4d10f8991c3063fcdcc2247e729f8991924
GPlay97 <GPlay97@users.noreply.github.com>
2019-01-03 23:48:02 +0100

    implementation of qr status and first handling

--- app/www/components/pages/dashboard-page.vue
+++ app/www/components/pages/dashboard-page.vue
@@ -21,7 +21,7 @@
         <v-layout justify-center align-center>
           <v-flex shrink>
 
-            <v-card>
+            <v-card v-if="qr">
               <v-img src="images/background2-min.jpg" aspect-ratio="2.75"></v-img>
 
               <v-card-title primary-title>
@@ -29,15 +29,17 @@
                   <h3 class="headline mb-0 charging">Charging</h3><br>
                   <div>
                     <div style="float: left; margin-bottom: 4px">
-                      <v-progress-circular :rotate="-90" :size="100" :width="15" :value="10" color="#fc9928">
-                        10 %
+                      <v-progress-circular :rotate="-90" :size="100" :width="15" :value="qr.soc_bms || qr.soc_display || 0"
+                        color="#fc9928">
+                        {{ qr.soc_bms || qr.soc_display || 0 }} %
                       </v-progress-circular>
                     </div>
                     <div style="margin-left: 4px;float: right;text-align: left;">
                       <div>
-                        <h4>Estimated time left (full charge): </h4> 00:28h
+                        <h4>Estimated time left (full charge): </h4> {{ qr.estimated_time || '00:00' }}h
                       </div>
-                      <div>
+                      <!-- TODO: Not available yet -->
+                      <div v-if="false">
                         <h4>Estimated time left (75% user notification): </h4> 00:17h
                       </div>
                     </div>
@@ -46,18 +48,48 @@
               </v-card-title>
 
               <v-card-actions>
-                <v-btn flat color="orange">Notify me on charge finished</v-btn>
+                <v-btn flat color="orange" @click="dialog = true">Notify me on charge finished</v-btn>
               </v-card-actions>
               <v-card-actions>
-                <v-btn flat color="orange">Notify driver that you need to charge</v-btn>
+                <v-btn flat color="orange" @click="dialog = true">Notify driver that you need to charge</v-btn>
               </v-card-actions>
             </v-card>
+
+            <v-layout row justify-center>
+              <v-dialog v-model="dialog" persistent max-width="290">
+                <v-card>
+                  <v-card-title class="headline">Unavailable</v-card-title>
+                  <v-card-text>This feature is currently under development and will be activated as soon as possible. Please be patient.</v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" flat @click="dialog = false">Ok, cool</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-layout>
+
+            <v-alert :value="loading" type="info">
+              Retrieving information from QR code. Please wait.
+            </v-alert>
+
+            <v-alert :value="!loading && denied" type="warning">
+              The driver is currently not charging or the data is too old.
+            </v-alert>
+
+            <v-alert :value="aborted && !denied" type="error">
+              Information could not be resolved.
+            </v-alert>
+
+            <v-alert :value="notFound" type="error">
+              QR Code not found.
+            </v-alert>
+
           </v-flex>
         </v-layout>
       </v-container>
     </v-content>
     <v-footer app fixed>
-      <span style="margin: auto">&copy; 2018 EVNotify - <a href="https://evnotify.de/imprint.html">Legal Notice</a></span>
+      <span style="margin: auto">&copy; 2019 EVNotify - <a href="https://evnotify.de/imprint.html">Legal Notice</a></span>
     </v-footer>
   </v-app>
 </template>
@@ -65,10 +97,30 @@
 <script>
   export default {
     data: () => ({
-      drawer: null
+      drawer: null,
+      qr: false,
+      dialog: false,
+      loading: true,
+      denied: false,
+      aborted: false,
+      notFound: false
     }),
-    props: {
-      source: String
+    mounted() {
+      this.$http.get(RESTURL + 'qr', {
+        params: {
+          code: location.search.split('code=')[1]
+        }
+      }).then(response => {
+        console.log(response);
+        this.loading = this.aborted = this.notFound = this.denied = false;
+        this.qr = response.body;
+      }, err => {
+        console.log(err);
+        this.loading = this.qr = false;
+        this.aborted = err.status !== 404;
+        this.notFound = err.status === 404;
+        this.denied = err.status === 401;
+      });
     }
   }
 </script>
